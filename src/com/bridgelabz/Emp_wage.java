12dad779008a123431b5a9a582abdaada11a6723
nehachougule1998 <nchougule734@gmail.com>
2022-03-02 17:20:30 +0530

    UC3 added

--- src/com/bridgelabz/Emp_wage.java
+++ src/com/bridgelabz/Emp_wage.java
@@ -5,22 +5,31 @@ public class Emp_wage {
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to employee_wage computation");
-	     int present = 0;
-         int absent = 1;
+	     int full_time = 1;
+         int part_time = 2;
          int wage_per_hrs = 20;
          int full_day_hrs = 8;
-         int empwage;
-         int check=(int)Math.floor(Math.random() *10) % 2;
+         int part_time_hrs = 4;
+         int wage;
+         int check=(int)Math.floor(Math.random() *10) % 3;
          
-         if (check==present) {
-             System.out.println("Employee is present."); //if check=0 than employee present
-             empwage = full_day_hrs * wage_per_hrs;
-             System.out.println("emp wage : " +empwage);
+         if (check==full_time) {
+             System.out.println("Employee is full time present"); 
+             wage = full_day_hrs * wage_per_hrs;
+             System.out.println("emp wage : " +wage);
         }
-         else
-         {
-             System.out.println("Employee is absent."); //if check=1 than employee absent
+         else if (check==part_time) 
+	    {
+             System.out.println("Employee is part time present"); 
+             wage = part_time_hrs * wage_per_hrs;
+             System.out.println("emp wage : " +wage);
+             
+        } 
+         else 
+         {	 
+        	System.out.println("employee is absent");
         }
+         
 	    
      }
 	
