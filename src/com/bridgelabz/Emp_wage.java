422859de97f008bb17445dbd1ba9c8939fb3e999
nehachougule1998 <nchougule734@gmail.com>
2022-03-02 16:43:33 +0530

    UC2 added

--- src/com/bridgelabz/Emp_wage.java
+++ src/com/bridgelabz/Emp_wage.java
@@ -5,13 +5,17 @@ public class Emp_wage {
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to employee_wage computation");
-	    
 	     int present = 0;
          int absent = 1;
+         int wage_per_hrs = 20;
+         int full_day_hrs = 8;
+         int empwage;
          int check=(int)Math.floor(Math.random() *10) % 2;
-
+         
          if (check==present) {
              System.out.println("Employee is present."); //if check=0 than employee present
+             empwage = full_day_hrs * wage_per_hrs;
+             System.out.println("emp wage : " +empwage);
         }
          else
          {
