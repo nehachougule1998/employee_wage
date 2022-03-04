40e2a5f7e4958d2ba29013b1274a62f34e624987
nehachougule1998 <nchougule734@gmail.com>
2022-03-04 14:20:55 +0530

    UC6 added

--- src/com/bridgelabz/Emp_wage.java
+++ src/com/bridgelabz/Emp_wage.java
@@ -3,19 +3,31 @@ import java.util.Random;
 import java.util.Scanner;
 public class Emp_wage {
     static final int full_time = 1;
-    static final int part_time =2;
-   public static void main(String[] args) {
+    static final int part_time = 2;
+ public static void main(String[] args) {
 		 System.out.println("Welcome to employee_wage computation");
+		 
 	     int wage_per_hrs = 20;
          int full_day_hrs = 8;
          int part_time_hrs = 4;
          int working_days =20;
          int total_Wage = 0;
          int wage;
+         int monthlyWage=0;
+         int hours=0;
+         int days=1;
          
-         for (int day = 1; day <= working_days; day++) {
+         while(days<20 && hours<100) {
+     		days++;
          int check=(int)Math.floor(Math.random() *10) % 3;
-         
+         int remaining_Hours=100-hours;
+         if(remaining_Hours%8<1) {
+        	 System.out.println("Employee is part time present"); 
+             wage = part_time_hrs * wage_per_hrs;
+             System.out.println("emp wage : " +wage);
+             total_Wage  = total_Wage + wage;
+        	 
+         }
          switch (check) {
          case full_time : 
              System.out.println("Employee is full time present"); 
@@ -37,6 +49,9 @@ public class Emp_wage {
             
         }
          System.out.println("Total wage for a month is " + total_Wage);
+         System.out.println("Total working hours : "+ hours);
+     	 System.out.println("total working days : "+ days);
+     	 
     }
 }
 
